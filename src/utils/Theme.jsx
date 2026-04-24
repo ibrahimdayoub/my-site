10c4ab5f6803a3cbbf7026691faf411c98853324
ibrahimdayoub <ibrahimdayoub619@gmail.com>
2026-04-24 14:13:18 +0300

    Migrate deployment from Render to Netlify

--- src/utils/Theme.jsx
+++ src/utils/Theme.jsx
@@ -1,6 +1,6 @@
 export const theme = {
     colors: {
-        background: '#f00',//'#02050c',
+        background: '#02050c',
         primary: '#0f172a',
         primary_light: '#131e2f33',
         secondary: '#7eadfc',
